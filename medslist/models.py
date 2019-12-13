9d43ff2b9f02134f9231ebef2f8846910ceb8f19
joost soeterbroek <joost.soeterbroek@gmail.com>
2019-12-13 20:41:58 +0100

    initial commit

--- medslist/models.py
+++ medslist/models.py
@@ -4,9 +4,10 @@ import uuid
 import pickle
 import base64
 import numpy as np
+import pandas as pd
 from django.db import models
 from django.core.exceptions import ValidationError
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 #from django.utils.dateparse import parse_datetime
 from django.utils import timezone
 #from django.db.models.signals import post_save
@@ -43,17 +44,17 @@ class Drug(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     created = models.DateTimeField(auto_now_add=True, editable=False)
     name = models.CharField("naam", max_length=255,
-        help_text="De naam van het medicijn")
+                            help_text="De naam van het medicijn")
     ingredient = models.CharField(max_length=255)
     use = models.CharField("Te gebruiken bij", max_length=255, help_text="")
     sideeffects = models.TextField("Mogelijke bijwerkingen",
-        help_text="Mogelijke bijwerkingen van dit medicijn")
+                                   help_text="Mogelijke bijwerkingen van dit medicijn")
     particularities = models.CharField(
         "Bijzonderheden", max_length=255, help_text="Bijzonderheden")
     appearance = models.CharField(
         "Uiterlijk", max_length=255, help_text="Uiterlijk")
     intake = models.CharField("Inname", max_length=255,
-        help_text="Wijze van inname")
+                              help_text="Wijze van inname")
     lastmod = models.BooleanField("is last modified", default=False)
     lastmod_who = LastUserField(
         on_delete=models.PROTECT, related_name="drug_lastmod_who")
@@ -157,9 +158,22 @@ class Client(models.Model):
         verbose_name = 'Client'
         verbose_name_plural = 'Clienten'
 
+class PrescriptionManager(models.Manager):
+    """ Prescription Model Manager """
+
+    def create_prescription(self, name, client, doctor, drug, remarks, start_date, end_date):
+        #initial 2-d numpy matrix 42 range, seven days, six periods
+        arr = np.arange(42)
+        zz = np.zeros(arr.shape)
+        npmatrix = zz.reshape(6, 7) # six periods, seven days
+        matrix_bytes = pickle.dumps(npmatrix)
+        matrix_base64 = base64.b64encode(matrix_bytes)
+        matrix = matrix_base64
+        p = self.create(name=name, client=client, doctor=doctor, drug=drug, remarks=remarks, start_date=start_date, end_date=end_date, matrix=matrix)
+        return p
 
 class Prescription(models.Model):
-    """"""
+    """ Prescription Model """
 
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=250)
@@ -185,9 +199,54 @@ class Prescription(models.Model):
     doublecheck_when = models.DateTimeField(
         "date is doublechecked", null=True, blank=True)
     history = HistoricalRecords()
+    objects = PrescriptionManager()
 
     def __str__(self):
         return self.name
+
+    def update_matrix(self, period, day, value):
+        """
+        takes 3 arguments:
+         - period (one of: 'p00100','p00200','p00300','p00400','p00500','p00600') 
+         - day (one of: 'mon','tue','wed','thu','fri','sat','sun')
+         - value
+        e.g.:
+        p1.update_matrix('p00400', 'sat', 12) # value 12 on 4th period on saturday
+        """
+        valid_periods = {'p00100','p00200','p00300','p00400','p00500','p00600'}
+        if period not in valid_periods:
+            raise ValueError("results: period must be one of %s." % valid_periods)
+        valid_days = {'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'}
+        if day not in valid_days:
+            raise ValueError("results: day must be one of %s." % valid_days)
+        df = self.get_pdfmatrix()
+        df.at[period, day] = value
+        self.set_pdfmatrix(df)
+
+    def get_npmatrix(self):
+        """retrieve matrix from database, return a numpy 2d array"""
+        matrix_bytes = base64.b64decode(self.matrix)
+        npmatrix = pickle.loads(matrix_bytes)
+        return npmatrix
+
+    def get_pdfmatrix(self):
+        """argument numpy matrix, return a pandas dataframe"""
+        _npmatrix = self.get_npmatrix()
+        column_names = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+        row_names = ['p00100', 'p00200', 'p00300', 'p00400', 'p00500', 'p00600']
+        pdfmatrix = pd.DataFrame(_npmatrix, columns=column_names, index=row_names)
+        return pdfmatrix
+
+    def set_npmatrix(self, _npmatrix):
+        """set numpy matrix to db"""
+        matrix_bytes = pickle.dumps(_npmatrix)
+        matrix_base64 = base64.b64encode(matrix_bytes)
+        self.matrix = matrix_base64
+
+    def set_pdfmatrix(self, _pdfmatrix):
+        """convert pandas dataframe to numpy, then set to db"""
+        _npmatrix = _pdfmatrix.to_numpy()
+        self.set_npmatrix(_npmatrix)
 
     def is_lastmod(self):
         return self.lastmod
@@ -200,7 +259,7 @@ class Prescription(models.Model):
 
     def set_lastmod(self, who):
 
-        # when a modification is made, doublecheck flag has to be 
+        # when a modification is made, doublecheck flag has to be
         # set back to false
         if self.is_doublecheck():
             self.doublecheck = False
@@ -221,28 +280,14 @@ class Prescription(models.Model):
 
     def set_doublecheck(self, who):
 
-        # do not allow the lastmod user to be same as 
+        # do not allow the lastmod user to be same as
         # doublecheck user
         if self.lastmod_who == who:
             raise ValidationError(_('lastmod user cannot be the same as doublecheck user.'))
-
         self.doublecheck_who = who
         self.doublecheck_when = timezone.now()
         self.doublecheck = True
         return self.doublecheck
-
-    def set_matrix(self):
-    #    arr = np.arange(42)
-    #    matrix = arr.reshape(7, 6) # seven days, six periods
-        matrix_bytes = pickle.dumps(matrix)
-        matrix_base64 = base64.b64encode(matrix_bytes)
-        self.model.matrixfield = matrix_base64
-        return self.model.matrixfield
-
-    def get_matrix(self):
-       matrix_bytes = base64.base64decode(self.model.matrixfield)
-       self.matrix = pickle.loads(matrix_bytes)
-       return self.matrix
 
     class Meta:
         # abstract = True
--- tests/test_models.py
+++ tests/test_models.py
@@ -11,8 +11,8 @@ class DrugModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         #print("setUpTestData: Run once to set up non-modified data for all class methods.")
-        u1 = User.objects.create_user(username='user1', password='top_secret')
-        u2 = User.objects.create_user(username='user2', password='top_secret')
+        #u1 = User.objects.create_user(username='user1', password='top_secret')
+        #u2 = User.objects.create_user(username='user2', password='top_secret')
         d1 = Drug.objects.create(
             name='paracetamol', ingredient='paracetamol', use='headache',
             sideeffects='more headaches', particularities='geen'
@@ -22,7 +22,11 @@ class DrugModelTest(TestCase):
             firstname='firstname', lastname='testclient',
             dateofbirth='1968-01-22', bsn='122475243'
         )
-        Prescription.objects.create(
+        c2 = Client.objects.create(
+            firstname='firstname', lastname='testclient2',
+            dateofbirth='1968-01-22', bsn='122475243'
+        )
+        Prescription.objects.create_prescription(
             name='prescription test one',
             client=c1,
             drug=d1,
@@ -31,6 +35,15 @@ class DrugModelTest(TestCase):
             start_date='2019-11-10',
             end_date='2019-12-30'
         )
+        Prescription.objects.create_prescription(
+            name='prescription test two',
+            client=c2,
+            drug=d1,
+            doctor=doc1,
+            remarks='remark',
+            start_date='2019-11-10',
+            end_date='2019-12-30'
+	)
 
     def setUp(self):
         #print("setUp: Run once for every test method to setup clean data.")
@@ -73,16 +86,16 @@ class DrugModelTest(TestCase):
         p1.name = "modified prescription test one for doublecheck"
         p1.set_lastmod(u3)
         p1.save()
-        # #1
+        # subtest #1
         with self.assertRaises(ValidationError):
             p1.set_doublecheck(u3)
         p1.save()
-        # #2
+        # subtest #2
         p1.set_doublecheck(u4)
         p1.save()
         is_doublecheck = p1.is_doublecheck()
         self.assertEqual(is_doublecheck, True)
-        # #3
+        # subtest #3
         p1.name = "modified prescription test one for doublecheck again"
         p1.set_lastmod(u3)
         p1.save()
@@ -90,8 +103,36 @@ class DrugModelTest(TestCase):
         self.assertEqual(is_doublecheck, False)
 
     def test_prescription_matrix(self):
+        """
+        scenario:
+          - create a prescription with a (empty) matrix 
+          - #1 get and set numpy empty matrix
+          - #2 get matrix as pandas dataframe, change value and
+               set back to db
+          - #3 do the same via method update_matrix (which will be used 
+               in views)
+        """
+
         print("test_prescription_matrix: test prescription matrix")
-        pass
+        p1 = Prescription.objects.get(id=1)
+        # subtest #1
+        npmatrix_1 = p1.get_npmatrix()
+        p1.set_npmatrix(npmatrix_1)
+        npmatrix_2 = p1.get_npmatrix()
+        self.assertEqual(npmatrix_1.all(), npmatrix_2.all())
+        # subtest #2
+        pd_dataframe1 = p1.get_pdfmatrix()
+        pd_dataframe1.at['p00500','mon'] = 10
+        p1.set_pdfmatrix(pd_dataframe1)
+        # subtest #3
+        v1 = 12
+        with self.assertRaises(ValueError):
+            p1.update_matrix('10400', 'sat', v1) # should raise value error (wrong period)
+        p1.update_matrix('p00400', 'sat', v1) # value 12 on 4th period on saturday
+        np = p1.get_npmatrix()
+        v2 = int(np[3][5])
+        self.assertEqual(v1, v2)
+
 
     def test_prescription_auditlog(self):
         print("test_prescription_auditlog: test prescription auditlog")
