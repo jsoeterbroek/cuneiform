e83e3237c64defdb228a9ee02f57374b87249a40
joost soeterbroek <joost.soeterbroek@gmail.com>
2020-02-03 16:25:05 +0100

    fix celery

--- signoff/celery.py
+++ signoff/celery.py
@@ -1,24 +1,10 @@
-from __future__ import absolute_import, unicode_literals
+
 import os
 from celery import Celery
-from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cuneiform.settings')
-app = Celery('picha', include=['signoff.pe'])
-
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-
+app = Celery('cuneiform')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-# app.autodiscover_tasks()
-
-# Optional configuration, see the application user guide.
-app.conf.update(
-    result_expires=3600,
-)
-
-if __name__ == '__main__':
-    app.start()
+app.autodiscover_tasks()
